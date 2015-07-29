b5606df5b45813875ec6e0a3b8dc86cba2f4f194
ngamby <noah@gamboafamily.com>
2015-07-29 01:45:23 -0700

    first commit

--- calculator/CalculatorBrain.swift
+++ calculator/CalculatorBrain.swift
@@ -9,62 +9,198 @@
 import Foundation
 
 class CalculatorBrain {
-    private enum Op {
+    private enum Op: Printable {
         case Operand(Double)
-        case UnaryOperation(String, Double->Double)
+        case Constant(String, Double)
+        case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
+        case openParen(String)
+        case closeParen(String)
+
+        var isOperation: Bool {
+            switch self {
+            case .UnaryOperation, .BinaryOperation:
+                return true
+            default:
+                return false
+            }
+        }
+        
+        var isOpenParen: Bool {
+            switch self {
+            case .openParen:
+                return true
+            default:
+                return false
+            }
+        }
+
+        
+        var precedence: Int {
+            switch self {
+            case .BinaryOperation(let val,_):
+                if(val == "×" || val == "÷") {return 2}
+                else {return 1}
+            case .UnaryOperation(let val, _):
+                return 0
+            default:
+                return 0
+            }
+        }
+        
+        var associativity: String {
+            switch self {
+            case .BinaryOperation(let val, _):
+                if val == "^" {return "right"}
+                else {return "left"}
+            default: return "left"
+            }
+            
+        }
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .Constant(let symbol, _):
+                    return symbol
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                case .openParen(let symbol):
+                    return symbol
+                case .closeParen(let symbol):
+                    return symbol
+                }
+            }
+        }
     }
+    
     private var opStack = [Op]()
     
+    private var infixOpStack = [Op]()
     
     private var knownOps = [String:Op]()
     
     init() {
-        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
-        knownOps["+"] = Op.BinaryOperation("+") { $0 + $1 }
-        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
         knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷", { $1 / $0 })
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["-"] = Op.BinaryOperation("-", { $1 - $0 })
         knownOps["√"] = Op.UnaryOperation("√", sqrt)
-    }
-    private func evaluate(ops: [Op]) -> (result: Double?, remaingOps: [Op]) {
+        knownOps["sin"] = Op.UnaryOperation("sin", sin)
+        knownOps["cos"] = Op.UnaryOperation("cos", cos)
+        knownOps["π"] = Op.Constant("π", M_PI)
         
+        knownOps["("] = Op.openParen("(")
+        knownOps[")"] = Op.closeParen(")")
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
         if !ops.isEmpty {
-            var remainingOps = ops // makes a copy of ops (sorta) dont worry about it
-            let op = remainingOps.removeLast() //this throws an error because ops is immutable
-            //cannot be done because this is still a copy
+            var remainingOps = ops;
+            let op = remainingOps.removeLast()
             switch op {
-                case .Operand(let operand): return (operand, remainingOps)
-                case .UnaryOperation(_, let operation): //underbar is universal idk
-                    let operandEvaluation = evaluate(remainingOps)
-                    if let operand = operandEvaluation.result {
-                        return (operation(operand), operandEvaluation.remaingOps)
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .Constant(_, let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    var result = operation(operand)
+                    return (result, operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let operand1Evaluation = evaluate(remainingOps)
+                if let operand1 = operand1Evaluation.result {
+                    let operand2Evaluation = evaluate(operand1Evaluation.remainingOps)
+                    if let operand2 = operand2Evaluation.result {
+                        var result = operation(operand1, operand2)
+                        return (result, operand2Evaluation.remainingOps)
                     }
-                case .BinaryOperation(_, let operation):
-                    let op1Evaluation = evaluate(remainingOps)
-                    if let operand1 = op1Evaluation.result {
-                        let op2Evaluation = evaluate(op1Evaluation.remaingOps)
-                        if let operand2 = op2Evaluation.result {
-                            return (operation(operand1, operand2), op2Evaluation.remaingOps)
-                        }
-                    }
+                }
+            default:
+                return (nil, remainingOps)
             }
-            
-            
         }
         return (nil, ops)
     }
-    func evaluate() -> Double? {
-        let (result, _ ) = evaluate(opStack)
-        return result
-    }
-    func pushOperand(operand: Double) -> Double? {
-        opStack.append(Op.Operand(operand))
-        return evaluate()
-    }
-    func performOperation(symbol: String) -> Double? {
-        if let operation = knownOps[symbol] {
-            opStack.append(operation)
+    
+    
+    private struct Stack {
+        var items = [Op]()
+        mutating func push(item: Op) {
+            items.append(item)
         }
-        return evaluate()
+        mutating func pop() -> Op {
+            return items.removeLast()
+        }
+        func peek() -> Op? {
+            return items.last
+        }
     }
+    
+    private func infixToRPN(ops: [Op]) -> [Op] {
+        var opStack = Stack()
+        var returnOps = [Op]()
+        for op in ops {
+            switch op {
+            case .Constant:
+                returnOps.append(op)
+            case .Operand:
+                returnOps.append(op)
+            case .BinaryOperation, .UnaryOperation:
+                var top = opStack.peek()
+                while top != nil && top?.isOperation == true &&
+                ((op.associativity == "left" && op.precedence <= top?.precedence) ||
+                (op.associativity == "right" && op.precedence < top?.precedence))
+                {
+                    returnOps.append(opStack.pop())
+                    top = opStack.peek()
+                }
+                opStack.push(op)
+            case .openParen:
+                opStack.push(op)
+            case .closeParen:
+                opStack.pop() // remove close paren
+                var top = opStack.peek()
+                while top?.isOpenParen != true && top != nil {
+                    returnOps.append(opStack.pop())
+                    top = opStack.peek()
+                }
+                if top != nil {
+                    opStack.pop() // remove open paren
+                }
+            }
+        }
+        while opStack.peek() != nil {
+            returnOps.append(opStack.pop())
+        }
+        return returnOps
+    }
+    
+    func isOp(op: String) -> Bool {
+        return knownOps[op] != nil
+    }
+    
+    func pushOperation(op: String) {
+        infixOpStack.append(knownOps[op]!)
+    }
+    
+    func pushOperand(val: Double) {
+        infixOpStack.append(Op.Operand(val))
+    }
+    
+    func evaluate() -> Double? {
+        println(infixOpStack);
+        opStack = infixToRPN(infixOpStack)
+        let result = evaluate(opStack)
+        infixOpStack = [Op]()
+        opStack = [Op]()
+        return result.result!
+    }
+
 }
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -15,85 +15,49 @@ class ViewController: UIViewController { // single inheritance only
     var brain = CalculatorBrain()
     
     @IBOutlet weak var Display: UILabel! //? vs ! doesn't change the type, changes the usage
-    //this is an implicitly unwrapped optional
-    //things that are set very early and stay that way, might be good to always unwrap the value
-    //all instances live in the heap and memory is managed for you
-    //its not garbage collection, its reference counting
-    //memory management is done for us. 
-    //no syntax that says this a pointer. objects are always pointers.
-    //exclamation point does something else.
-    // "UILabel" is the type
-    var UserIsInTheUserOfTypingANumber = false
-    //we can remove ": bool" because value is inferred
     
+    var UserIsInTheMiddleOfTypingANumber: Bool = false
+    //we can remove ": bool" because value is inferred
+    var userIsAboutToEnterANewCalculation: Bool = true
+    
+    var numStr: String = ""
+    //if the char pushed is an integer,
     @IBAction func appendDigit(sender: UIButton) {
         //func = says its a method
         // "sender" name of param, "UIButton" = type of param
-        let digit = sender.currentTitle!
-        if UserIsInTheUserOfTypingANumber {
-            Display.text = Display.text! + digit
+        let char = sender.currentTitle!
+        if userIsAboutToEnterANewCalculation {
+            Display.text = ""
+            userIsAboutToEnterANewCalculation = false
+        }
+        if brain.isOp(char) {
+            if let numFormat = NSNumberFormatter().numberFromString(numStr) {
+                brain.pushOperand(numFormat.doubleValue)
+            }
+            brain.pushOperation(char)
+            numStr = ""
+            Display.text = Display.text! + char
+        }
+        else if UserIsInTheMiddleOfTypingANumber {
+            numStr = numStr + char
+            Display.text = Display.text! + char
              //display.text is an optional so you need to unwrap it
         } else {
-            Display.text = digit
-            UserIsInTheUserOfTypingANumber = true
-        }
-        //what does optional mean when this is printed?
-        //  you never put a type for digit, but swift is great at type inference. it makes it the same type as what you assign it to be. the type is called ? = Optional. it can have two values: notSet (nil) or something.
-        // the exclamation point unwraps that optional and gets the string from currentTitle.
-        // let is the same as var but let is a constant
-        
-       
-    }
-    
-    //This is the C portion of the MVC
-//    func performOperation(operation: (Double, Double) -> Double)
-//    {
-//        if(operandStack.count >= 2) {
-//            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-//            enter();
-//        }
-//    }
-//    func performOperation(operation: Double -> Double)
-//    {
-//        if(operandStack.count >= 1) {
-//            displayValue = operation(operandStack.removeLast())
-//            enter();
-//        }
-//    }
-    @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
-        if(UserIsInTheUserOfTypingANumber) {
-            enter()
-        }
-        if let operation = sender.currentTitle {
-            if let result = brain.performOperation(operation) {
-                displayValue = result
-            } else {
-                displayValue = 0;
-            }
+            numStr = char
+            Display.text = Display.text! + char
+            UserIsInTheMiddleOfTypingANumber = true
         }
     }
-    
-    
-//    var operandStack = Array<Double>()
     
     @IBAction func enter() {
-        UserIsInTheUserOfTypingANumber = false
-        var displayValue: Double {
-            get { // gets the value you want from something
-                return NSNumberFormatter().numberFromString(Display.text!)!.doubleValue
-            }
-            set { // set that value to the value you want to put
-                Display.text = "\(newValue)"
-                UserIsInTheUserOfTypingANumber = false
+        if UserIsInTheMiddleOfTypingANumber  {
+            if let numFormat = NSNumberFormatter().numberFromString(numStr) {
+                brain.pushOperand(numFormat.doubleValue)
             }
         }
-        if let result  = brain.pushOperand(displayValue) {
-        brain.pushOperand(displayValue)
-        }
-        else  {
-            displayValue = 0
-        }
+        UserIsInTheMiddleOfTypingANumber = false
+        Display.text = NSNumberFormatter().stringFromNumber(brain.evaluate()!)!
+        userIsAboutToEnterANewCalculation = true
         
     }
 
